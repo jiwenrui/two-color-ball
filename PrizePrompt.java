30257ddd0c5aaf87eefd823c5a338013038fd25b
Green bump <Erangluang@163.com>
2018-10-26 16:24:41 +0800

    Create PrizePrompt.java

---
+++ PrizePrompt.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+
+
+public class PrizePrompt {
+	
+	public PrizePrompt() {
+
+
+		JOptionPane.showMessageDialog(null,"           congratulations!","results",JOptionPane.INFORMATION_MESSAGE);
+			
+	}
+  
+}
