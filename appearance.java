8ebf0f00e1e8dfca64d56c60d04e64cfbac6e01d
Green bump <Erangluang@163.com>
2018-10-26 16:27:39 +0800

    Update appearance.java

--- appearance.java
+++ appearance.java
@@ -3,132 +3,138 @@ package gui;
 import javax.swing.*;
 
 
-public class Appearance {
+public class Appearance extends JFrame{
 
+	//red balls and blue balls to prompt
 	private JLabel redLabel = new JLabel("red balls");
 	private JLabel blueLabel = new JLabel("bule balls");
-    private static JTextField redText1 = new JTextField(12);			//这里的两个static是没办法了，笑哭
-	private static JTextField redText2 = new JTextField(12);
-	private static JTextField redText3 = new JTextField(12);
-	private static JTextField redText4 = new JTextField(12);
-	private static JTextField redText5 = new JTextField(12);
-	private static JTextField redText6 = new JTextField(12);
 
-	private static JTextField blueText1 = new JTextField(12);
-	private static JTextField blueText2 = new JTextField(12);
-
-	private  JLabel prompt = new JLabel("choose six red ball from 1 to 33:");
+	//prompt messages to guide user to enter
+	private static JLabel prompt = new JLabel("choose six red ball from 1 to 33:");
+	//input'text to let users input messages here
 	private static JTextField input = new JTextField("enter number here");
-	//private JLabel Prompt = new JLabel("choose two blue ball from 1 to 16:");
+	//prize mount will comes up here
 	private JTextField prizeText = new JTextField();
 
-	private JButton beginButton = new JButton("start");
-	private JButton stopButton = new JButton("stop");
-	private JButton aboutButton = new JButton("statement");
+	//three buttons
+	private JButton beginButton = new JButton("submit");
+	private JButton lotteryButton = new JButton("Lottery");
+	private JButton statementButton = new JButton("statement");
 
-		//这里我把构造方法重写了
+
+	//to record times
+	public static int times = 1;
+
+	//seven balls numbers text, but here I defined eight JTextField, because I want to use "1-7" directly!
+	public static JTextField[] ballsNumberText ={
+			new JTextField(12),
+			new JTextField(12),
+			new JTextField(12),
+			new JTextField(12),
+			new JTextField(12),
+			new JTextField(12),
+			new JTextField(12),
+			new JTextField(12),
+	};
+
+		//here is constructor
         public Appearance(){
 
-        	JPanel panel = new JPanel();
-        	panel.setLayout(null);						//make the layout manager empty
+			//serve as subcontainer
+			JPanel panel = new JPanel();
+			//make the layout manager empty
+			panel.setLayout(null);
 
+        	//red and blue labels' locations
 	        redLabel.setBounds(10,20,60,55);
-	        panel.add(redLabel);
+	        panel.add(redLabel);													//add it to container
 			//Enter the number of red chromosphere numbers purchased
 			blueLabel.setBounds(510,20,60,55);
 			panel.add(blueLabel);
 
-	        //Modify this according to the rules of the two-color ball
-	        redText1.setBounds(70,20,66,55);
-	        panel.add(redText1);
-	        redText1.setEditable(false);			//内容不可更改
-			redText2.setBounds(142,20,66,55);
-			panel.add(redText2);
-			redText2.setEditable(false);			//内容不可更改
-			redText3.setBounds(214,20,66,55);
-			panel.add(redText3);
-			redText3.setEditable(false);			//内容不可更改
-			redText4.setBounds(286,20,66,55);
-			panel.add(redText4);
-			redText4.setEditable(false);			//内容不可更改
-			redText5.setBounds(358,20,66,55);
-			panel.add(redText5);
-			redText5.setEditable(false);			//内容不可更改
-			redText6.setBounds(430,20,66,55);
-			panel.add(redText6);
-			redText6.setEditable(false);			//内容不可更改
+	        //these six are red balls
+			ballsNumberText[1].setBounds(70,20,66,55);
+	        panel.add(ballsNumberText[1]);
+	        ballsNumberText[1].setEditable(false);									//内容不可更改
+			ballsNumberText[2].setBounds(142,20,66,55);
+			panel.add(ballsNumberText[2]);
+			ballsNumberText[2].setEditable(false);									//内容不可更改
+			ballsNumberText[3].setBounds(214,20,66,55);
+			panel.add(ballsNumberText[3]);
+			ballsNumberText[3].setEditable(false);									//内容不可更改
+			ballsNumberText[4].setBounds(286,20,66,55);
+			panel.add(ballsNumberText[4]);
+			ballsNumberText[4].setEditable(false);									//内容不可更改
+			ballsNumberText[5].setBounds(358,20,66,55);
+			panel.add(ballsNumberText[5]);
+			ballsNumberText[5].setEditable(false);									//内容不可更改
+			ballsNumberText[6].setBounds(430,20,66,55);
+			panel.add(ballsNumberText[6]);
+			ballsNumberText[6].setEditable(false);									//内容不可更改
 
-	        //Modify this according to the rules of the two-color ball
-	        blueText1.setBounds(580,20,66,55);
-	        panel.add(blueText1);
-	        blueText1.setEditable(false);
-			blueText2.setBounds(652,20,66,55);
-			panel.add(blueText2);
-			blueText2.setEditable(false);
+	        //blue balls
+			ballsNumberText[7].setBounds(580,20,88,55);
+	        panel.add(ballsNumberText[7]);
+			ballsNumberText[7].setEditable(false);
+			/*ballsNumberText[8].setBounds(652,20,66,55);
+			panel.add(ballsNumberText[8]);
+			ballsNumberText[8].setEditable(false);*/
 
+			//here is prompt messages to let users know that you need to enter blue balls numbers
 	        prompt.setBounds(10,90,200,35);
 	        panel.add(prompt);
 
+	        //importantly! the text to input. It is the soul of whole program
 	        input.setBounds(205,90,120,35);
 	        panel.add(input);
 
+			//the location settings of prize mount
 	        prizeText.setBounds(380,100,240,55);
 	        panel.add(prizeText);
-	        //funcTest.prizeShow(prizeText);
 
-	        //Enter the number of bule chromosphere numbers purchased
+	        //three function buttons' locations
 	        beginButton.setBounds(190,210,100,25);
 	        panel.add(beginButton);
-	        stopButton.setBounds(320,210,100,25);
-	        panel.add(stopButton);
-	        aboutButton.setBounds(450,210,100,25);
-	        panel.add(aboutButton);
+	        lotteryButton.setBounds(320,210,100,25);
+	        panel.add(lotteryButton);
+	        statementButton.setBounds(450,210,100,25);
+	        panel.add(statementButton);
 
-	        JFrame frame = new JFrame("Two-color ball automatic lottery machine");
-	        JFrame.setDefaultLookAndFeelDecorated(true);//java style
+	        //create a window to contain JPanel
+	        JFrame frame = this;
+	        frame.setTitle("Two-color ball automatic lottery machine");							//title
+	        JFrame.setDefaultLookAndFeelDecorated(true);											//java style
 
-	        //make visible,base set
+	        //make visible, base set
 	        frame.setVisible(true);
 	        frame.setSize(800,300);
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setResizable(false);																//can not adjust
+			frame.setLocationRelativeTo(null);														//comes up on the center of screen
 
+			//add JPanel
 	        frame.add(panel);
     }
 
-        //得到组件的函数组
-	    public  JLabel getPrompt() {
+        //a set of functions to get units, and some are not used
+		//public JPanel getPanel(){ return panel; }
+	    public static JLabel getPrompt() {
 	    	return prompt;
 	    }
 
-	    public static JTextField getRedText1() {
-	    	return redText1;
-	    }
-		public static JTextField getRedText2() {
-			return redText2;
-		}
-		public static JTextField getRedText3() {
-			return redText3;
-		}
-		public static JTextField getRedText4() { return redText4; }
-		public static JTextField getRedText5() {
-			return redText5;
-		}
-		public static JTextField getRedText6() {
-			return redText6;
-		}
+	    /*public JTextField getRedText1() { return ballsNumberText[1]; }
+		public JTextField getRedText2() { return ballsNumberText[2]; }
+		public JTextField getRedText3() { return ballsNumberText[3]; }
+		public JTextField getRedText4() { return ballsNumberText[4]; }
+		public JTextField getRedText5() { return ballsNumberText[5]; }
+		public JTextField getRedText6() { return ballsNumberText[6]; }
 
-	    public static JTextField getBlueText1() {
-	    	return blueText1;
-	    }
-		public static JTextField getBlueText2() {
-			return blueText2;
-		}
+	    public JTextField getBlueText1() { return ballsNumberText[1]; }
+		public JTextField getBlueText2() { return ballsNumberText[2]; }*/
 
 	    public static JTextField getInput() {
 	    	return input;
 	    }
-
-
 
 	    public JTextField getPrizeText() {
 	    	return prizeText;
@@ -137,11 +143,13 @@ public class Appearance {
 	    public JButton getBeginButton() {
 	    	return beginButton;
 	    }
-	    public JButton getStopButton() {
-	    	return stopButton;
+	    public JButton getLotteryButton() {
+	    	return lotteryButton;
 	    }
-	    public JButton getAboutButton() {
-	    	return aboutButton;
+	    public JButton getStatementButton() {
+	    	return statementButton;
 	    }
 
 }
+
+
