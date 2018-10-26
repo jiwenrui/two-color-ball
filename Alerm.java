8e0c3443a9f6ea79ad944aa3837c920905471579
Green bump <Erangluang@163.com>
2018-10-26 16:23:46 +0800

    Create Alerm.java

---
+++ Alerm.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.*;
+
+public class Alerm {
+	
+	public Alerm() {
+		
+		JOptionPane.showMessageDialog(null,"             Please enter numbers","Alert",JOptionPane.ERROR_MESSAGE);
+			
+	}
+
+}
