d4ae8d43340ff02d245db0e848c4bc6e35770f99
Green bump <Erangluang@163.com>
2018-10-26 16:31:52 +0800

    Update statement.java

--- statement.java
+++ statement.java
@@ -1,18 +1,26 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class statement {
-    public statement(JPanel panel){
-        JLabel label = new JLabel("<html><body><p>developers: B17080513 jwr B17080522 sql B17080529 clf</p></br><p>time:2018-10-24</p></br><p>Contact information:824845410@qq.com<body></html>");
-        label.setBounds(10,20,1000,1000);
-        panel.add(label);
+public class statement extends JFrame{
+    public statement(){
+        JLabel label = new JLabel();
+        label.setText("<html><body><p>developers: B17080513 jwr B17080522 sql B17080529 clf</p></br><p>time:2018-10-24</p></br><p>Contact information:824845410@qq.com</p></body></html>");
+
+        Container container = getContentPane();
+        container.setLayout(new BorderLayout());
+        container.add(label,BorderLayout.CENTER);
+
+        statement frame = this;
         JFrame.setDefaultLookAndFeelDecorated(true);//java style
-        JFrame frame = new JFrame("Developer Statement");
         //make visible,base set
         frame.setVisible(true);
-        frame.setSize(300,300);
+        frame.setSize(400,260);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setResizable(false);
         //need a listener to make it connect with the button named statement
+        frame.setLocationRelativeTo(null);
+        //传入参数null 即可让JFrame 位于屏幕中央, 这个函数若传入一个Component ,则JFrame位于该组件的中央
     }
 }
